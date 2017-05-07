3d9e02a7c3b5390bce02dcef11d3c418b73033b5
Dominic Michalec <dominicjjmichalec@gmail.com>
2017-05-07 17:48:14 -0400

    clear up application helper for full_title and add comments to controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,17 @@
 class StaticPagesController < ApplicationController
+  # return the corresponding view to 'home' action
   def home
   end
 
+  # return the corresponding view to 'help' action
   def help
   end
 
+  # return the corresponding view to 'about' action
   def about
   end
 
+  # return the corresponding view to 'contact' action
   def contact
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # return the page title in the browser tab on a per-page basis
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title.blank? ? base_title : page_title + " | " + base_title
+  end
 end
