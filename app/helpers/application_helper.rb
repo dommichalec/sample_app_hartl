a8d330d4ef45216e4735445d8118d1b8d5bf3ee1
Dominic Michalec <dominicjjmichalec@gmail.com>
2017-05-08 19:39:16 -0400

    refactor site title and tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
+  # set the title of the entire application to constant SITE_TITLE
+  $SITE_TITLE = "Ruby on Rails Tutorial Sample App"
 
   # return the page title in the browser tab on a per-page basis
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-    page_title.blank? ? base_title : page_title + " | " + base_title
+    page_title.blank? ? $SITE_TITLE : page_title + " | " + $SITE_TITLE
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,32 +2,32 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
+  # return a 200 when the root url is received and assert title is correct
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "#{$SITE_TITLE}"
   end
 
+  # return a 200 when the help url is received and assert title is correct
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
+  # return a 200 when the about url is received and assert title is correct
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
+  # return a 200 when the contact url is received and assert title is correct 
   test "should get contact" do
     get contact_url
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
